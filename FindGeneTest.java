5bbbc987316906c438271a50c1a99427a9154268
mabbasiazad <m32abbas@uwaterloo.ca>
2023-12-22 15:16:05 -0500

    code handeling multiple TAA

--- FindGeneTest.java
+++ FindGeneTest.java
@@ -2,27 +2,22 @@ class FindGene {
     public String findGeneSimple(String s) {
         // start codon is "ATG"
         // stop codon is "TAA"
-        String result = "";
         int start_ptr = s.indexOf("ATG");
         if (start_ptr == -1) {
-            return "no gene found - no start";
+            return "";
         }
         
         int stop_ptr = s.indexOf("TAA", start_ptr + 3);
-        if (stop_ptr == -1) {
-            return "no gene found - no stop";
-        } 
 
-        if ((stop_ptr - start_ptr) % 3 == 0) {
-            result = s.substring(start_ptr, stop_ptr + 3);
+        while (stop_ptr != -1) {
+            if ((stop_ptr - start_ptr) % 3 == 0) {
+            return s.substring(start_ptr, stop_ptr + 3);
+            } else{ 
+                stop_ptr = s.indexOf("TAA", stop_ptr + 1);
+            }
         }
-        else {
-            result = "no gene found - invalid length";
-        }
-
-
-
-        return result; 
+            
+        return ""; 
     }
 }
 
@@ -31,13 +26,14 @@ class FindGene {
  */
 public class FindGeneTest {
     public static void main(String[] args) {
-        String dna_1 = "CGATGCGTATCCTGTAA"; 
-        String dna_2 = "ATGCGCGTAGT";
+        // String dna_1 = "CGATGCGTATCCTGTAA"; 
+        // String dna_2 = "ATGCGCGTAGT";
         String dna_3 = "ATGATCGCTAATGCTTAAGCTATG";
 
         FindGene findgene = new FindGene(); 
-        System.out.println(findgene.findGeneSimple(dna_1));
-        System.out.println(findgene.findGeneSimple(dna_2));
+        // System.out.println(findgene.findGeneSimple(dna_1));
+        // System.out.println(findgene.findGeneSimple(dna_2));
+        System.out.println(findgene.findGeneSimple(dna_3));
 
     }
 
